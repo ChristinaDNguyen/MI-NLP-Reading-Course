27c87693a3c6179dd94d025a5e50d345e74d8b4e
Christina Nguyen <christina.nguyen99@hotmail.com>
2022-04-21 12:48:33 -0400

    Delete LT.R

--- ResearchPaper/LT.R
+++
@@ -1,9 +0,0 @@
-#============================================================================= #
-#'A mere maiden': Exploring Lúthien Tinúviel's  relationship with dance and song
-#with tf-idf scores and fuzzy matching 
-#============================================================================= #
-
-#Section 1: tf-idf scoring ----------------------------------------------------
-
-
-#Section 2: fuzzy matching (ngrams) -------------------------------------------
